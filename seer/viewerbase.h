c811701ee301c0e0dcaf0c7b3308fccf6efe088d
corey.laptop <cc.seer@gmail.com>
2025-03-14 12:17:21 +0800

    Initial commit

---
+++ seer/viewerbase.h
@@ -0,0 +1,88 @@
+#pragma once
+
+#include <QBoxLayout>
+#include <QWidget>
+
+#include "viewoption.h"
+
+class ViewerBase : public QWidget {
+    Q_OBJECT
+public:
+    explicit ViewerBase(QWidget* parent = nullptr);
+    ~ViewerBase() override = default;
+
+    void load(QHBoxLayout* layout_control_bar,
+              std::unique_ptr<ViewOptions>&& ctx);
+
+    virtual QString name() const = 0;
+    // preferred size
+    // if it's image type, its size = 80000x80000, then return the val
+    virtual QSize getContentSize() const = 0;
+
+    virtual void loadFileInfo() {}
+    virtual void onCopyTriggered() {}
+
+    virtual void updateDPR(qreal /*dpr*/) {}
+    virtual void updateTheme(int /*theme*/) {}
+
+    Q_SIGNAL void sigCommand(int view_command_type, const QVariant& data = {});
+
+protected:
+    virtual void loadImpl(QBoxLayout* layout_content,
+                          QHBoxLayout* layout_control_bar)
+        = 0;
+    std::unique_ptr<ViewOptions> m_d;
+
+private:
+    class Impl {
+    public:
+        std::chrono::high_resolution_clock::time_point elapsed;
+        QVBoxLayout* layout;
+
+        Impl() : layout(new QVBoxLayout()) {}
+        ~Impl()
+        {
+            delete layout;
+        }
+    };
+
+    std::unique_ptr<Impl> m_impl;
+};
+
+#define ViewerBase_iid "seer.plugin.interface.preview/1.0"
+Q_DECLARE_INTERFACE(ViewerBase, ViewerBase_iid)
+
+///////////////////////////////////////////////
+inline ViewerBase::ViewerBase(QWidget* parent)
+    : QWidget(parent), m_impl(std::make_unique<Impl>())
+{
+    this->setLayout(m_impl->layout);
+    m_impl->layout->setSpacing(0);
+    m_impl->layout->setContentsMargins(0, 0, 0, 0);
+    connect(this, &ViewerBase::sigCommand, this, [this](auto t, const auto& v) {
+        if (t != ViewCommandType::VCT_StateChange) {
+            return;
+        }
+        if (v.toInt() != VCV_Loaded) {
+            return;
+        }
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(
+            end_time - m_impl->elapsed);
+        qDebug() << "[wnd] [timer] elapsed:" << this << duration.count()
+                 << "ms";
+    });
+}
+
+inline void ViewerBase::load(QHBoxLayout* layout_control_bar,
+                             std::unique_ptr<ViewOptions>&& ctx)
+{
+    emit sigCommand(ViewCommandType::VCT_StateChange, VCV_Loading);
+
+    m_impl->elapsed = std::chrono::high_resolution_clock::now();
+    m_d             = std::move(ctx);
+    qDebug() << "[wnd] [timer] started:" << this << m_d->d->is_main_wnd
+             << m_d->d->dpr;
+
+    loadImpl(m_impl->layout, layout_control_bar);
+}
---
+++ seer/viewoption.h
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <QSize>
+#include <QString>
+
+struct ViewOptionsData {
+    ViewOptionsData() : theme(-1), dpr(0.1), is_main_wnd(true), wnd_index(0) {}
+
+    // viewer name
+    QString type;
+    // absolute file path
+    QString path;
+    // lower case.
+    // When the file has no suffix, the suffix might be detected rather than
+    // taken from the path.
+    QString suffix;
+
+    QSize size_max_viewer;
+    QSize size_min_viewer;
+
+    // 1: dark, 0: light
+    int theme;
+    // scale factor
+    qreal dpr;
+
+    // main wnd has a different title bar
+    bool is_main_wnd;
+    // wnd_index == 0 => main wnd
+    ushort wnd_index;
+};
+
+class ViewOptions {
+public:
+    ViewOptions() : d(std::make_unique<ViewOptionsData>()) {}
+    virtual ~ViewOptions() {};
+
+    std::unique_ptr<ViewOptionsData> d;
+};
+using ViewOptionsPtr = std::unique_ptr<ViewOptions>;
+
+enum ViewCommandType : int {
+    VCT_StateChange = 0,        // ViewCommandValue_StateChange
+    VCT_OpenByDefaultSoftware,  // const QString& path
+    VCT_RevealInExplorer,       // const QString& path
+    VCT_AppendProperty,         // const QVector<QPair<QString, QString>>&
+
+    VCT_WiggleWindow,
+    VCT_ResizeWindow,
+    VCT_HideWindow,
+
+    VCT_ShowToastMsg,  // const QString& msg
+    VCT_HideToastMsg,
+
+    // the file property remains the same
+    VCT_LoadViewerWithNewPath,  // const QString& path
+    // the file property changes
+    VCT_LoadWindowWithNewPath = 10,  // const QString& path
+
+    VCT_LoadViewerWithNewType,  // const QString& viewer_name
+
+    VCT_LoadSeparated,  // const QString& path
+
+    // --------------------
+    VCT_Reserved_Max = 999
+};
+
+enum ViewCommandValue_StateChange : uchar {
+    // VCV_None = 0,
+    VCV_Loading,
+    VCV_Loaded,
+    VCV_Error
+};
