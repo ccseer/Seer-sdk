006698396c0d0f3a177c6f776225f06797f469bd
Seer <cc.seer@gmail.com>
2025-03-24 18:17:53 +0800

    Refactor embedplugin.h and ipc.h by removing unused definitions and comments

--- seer/embedplugin.h
+++ seer/embedplugin.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include <QDataStream>
-
 // send to seer
 #define SEER_OIT_MSG_W32 6500
 #define SEER_OIT_SUB_W32_LOAD_ERR 6501
@@ -21,50 +19,3 @@
 #define SERR_MSG_KEY_WID "window_index"
 #define SERR_MSG_KEY_SUB_ID "msg_sub_id"
 #define SERR_MSG_KEY_TOAST "toast_text"
-
-//////////////////////////
-/// bad idea
-#define SEER_OIT_MSG 6000
-#define SEER_OIT_SUB_LOAD_ERR 6001
-#define SEER_OIT_SUB_LOAD_OK 6002
-#define SEER_OIT_SUB_KEY_PRESS 6003
-#define SEER_OIT_SUB_WAGGLE 6004
-#define SEER_OIT_SUB_TOAST 6005
-// msg from seer
-// first msg from seer
-// then the following three msg will be sent immediately
-#define SEER_OIT_ATTACHED 6102
-// will be messaged anytime it changed
-#define SEER_OIT_SIZE_CHANGED 6101
-// will be messaged anytime it changed
-#define SEER_OIT_DPI_CHANGED 6100
-// only once, no preview window when changing theme setttings
-#define SEER_OIT_THEME_CHANGED 6103
-
-// TODO: OITData
-// struct MyData {
-//     int sub_type;
-//     int wnd_index;
-//    size_t data_size; 
-//    BYTE* data;        
-// };
-struct OITData {
-    // sub_type -> SEER_OIT_SUB_*
-    int sub_type  = 0;
-    int wnd_index = 0;
-    QByteArray data;
-};
-
-inline QDataStream& operator<<(QDataStream& out, const OITData& v)
-{
-    out << v.sub_type << v.wnd_index << v.data;
-    return out;
-}
-
-inline QDataStream& operator>>(QDataStream& in, OITData& v)
-{
-    in >> v.sub_type;
-    in >> v.wnd_index;
-    in >> v.data;
-    return in;
-}
--- seer/ipc.h
+++ seer/ipc.h
@@ -3,28 +3,23 @@
 #define SEER_CLASS_NAME L"SeerWindowClass"
 
 enum SeerCommand : int {
-  // Request the file path (sent from Seer)
-  SEER_REQUEST_PATH = 4000,
-  // Respond with the file path (send back to Seer)
-  SEER_RESPONSE_PATH = 4001,
+    // Request the file path (sent from Seer)
+    SEER_REQUEST_PATH = 4000,
+    // Respond with the file path (send back to Seer)
+    SEER_RESPONSE_PATH = 4001,
 
-  // Request a preview
-  // Note: The minimum interval between SEER_INVOKE_* commands should be 200ms
-  SEER_INVOKE_W32 = 5000,
-  // Preview in a separate window
-  SEER_INVOKE_W32_SEP = 5001,
+    // Request a preview
+    // Note: The minimum interval between SEER_INVOKE_* commands should be 200ms
+    SEER_INVOKE_W32 = 5000,
+    // Preview in a separate window
+    SEER_INVOKE_W32_SEP = 5001,
 
-  // Seer can have multiple windows(main + separated) at the same time
-  // Check if the main window is currently visible
-  SEER_IS_VISIBLE = 5004,
+    // Seer can have multiple windows(main + separated) at the same time
+    // Check if the main window is currently visible
+    SEER_IS_VISIBLE = 5004,
 
-  // Hide the main window if it is currently visible
-  SEER_HIDE = 5005,
-
-  // TODO: remove this
-  // deprecated
-  SEER_INVOKE_QT = 5002,
-  SEER_INVOKE_QT_SEP = 5003,
+    // Hide the main window if it is currently visible
+    SEER_HIDE = 5005,
 };
 
 // SEER_IS_VISIBLE
