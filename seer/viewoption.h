945f047cc67dffae228499de7dd7df22b5984c85
corey.laptop <cc.seer@gmail.com>
2025-07-19 17:44:48 +0800

    Update viewoption.h

--- seer/viewoption.h
+++ seer/viewoption.h
@@ -3,6 +3,10 @@
 #include <QSize>
 #include <QString>
 
+// viewer->property(g_property_*)
+inline constexpr auto g_property_key_plugin_mi  = "plugin_meta";
+inline constexpr auto g_property_key_plugin_ver = "plugin_version";
+
 struct ViewOptionsData {
     ViewOptionsData() : theme(-1), dpr(0.1), is_main_wnd(true), wnd_index(0) {}
 
@@ -32,7 +36,7 @@ struct ViewOptionsData {
 class ViewOptions {
 public:
     ViewOptions() : d(std::make_unique<ViewOptionsData>()) {}
-    virtual ~ViewOptions() {}
+    virtual ~ViewOptions() = default;
 
     std::unique_ptr<ViewOptionsData> d;
 };
