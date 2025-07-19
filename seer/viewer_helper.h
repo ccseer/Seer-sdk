d8a887d75c9ac2fb2b3de1f011e419f4adc8a65c
corey.laptop <cc.seer@gmail.com>
2025-07-19 17:25:28 +0800

    Update viewer_helper.h

--- seer/viewer_helper.h
+++ seer/viewer_helper.h
@@ -1,2 +1,26 @@
 #pragma once
 
+#include <qt_windows.h>
+
+#include <QDir>
+#include <QString>
+
+namespace seer {
+inline QString getDLLPath()
+{
+    MEMORY_BASIC_INFORMATION mbi = {};
+    VirtualQuery((void *)getDLLPath, &mbi, sizeof(mbi));
+    HMODULE hm = (HMODULE)mbi.AllocationBase;
+
+    QString dir;
+    TCHAR path[MAX_PATH] = {};
+    if (hm && GetModuleFileName(hm, path, MAX_PATH)) {
+        dir = QString::fromWCharArray(path);
+        dir = QFileInfo(dir).absoluteDir().absolutePath();
+    }
+    else {
+        qDebug() << "GetModuleFileName" << GetLastError();
+    }
+    return dir;
+}
+}  // namespace seer
