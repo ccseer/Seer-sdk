6ed8426c6e21ac552c0c48af6c9b1a58326754c9
corey.laptop <cc.seer@gmail.com>
2025-07-20 14:57:29 +0800

    Update viewer_helper.h

--- seer/viewer_helper.h
+++ seer/viewer_helper.h
@@ -2,14 +2,18 @@
 
 #include <qt_windows.h>
 
+#include <QCommandLineOption>
+#include <QCommandLineParser>
 #include <QDir>
-#include <QString>
+#include <QGuiApplication>
+#include <QScreen>
+#include <QSize>
 
 namespace seer {
 inline QString getDLLPath()
 {
     MEMORY_BASIC_INFORMATION mbi = {};
-    VirtualQuery((void *)getDLLPath, &mbi, sizeof(mbi));
+    VirtualQuery((void*)getDLLPath, &mbi, sizeof(mbi));
     HMODULE hm = (HMODULE)mbi.AllocationBase;
 
     QString dir;
@@ -19,8 +23,47 @@ inline QString getDLLPath()
         dir = QFileInfo(dir).absoluteDir().absolutePath();
     }
     else {
-        qDebug() << "GetModuleFileName" << GetLastError();
+        qDebug() << Q_FUNC_INFO << GetLastError();
     }
     return dir;
+}
+
+inline QSize parseViewerSizeFromConfig(const QStringList& args_raw)
+{
+    if (args_raw.isEmpty()) {
+        return {};
+    }
+    // arguments must start with the name of the executable
+    QStringList args{"dummy"};
+    args.append(args_raw);
+
+    QCommandLineParser parser;
+    QCommandLineOption opt_p{
+        QStringList() << "p" << "percent",
+        "Viewer window size as a percentage of the primary screen (1-100)",
+        "percent"};
+    QCommandLineOption opt_w{QStringList() << "w" << "width", "Viewer width",
+                             "width"};
+    QCommandLineOption opt_h{QStringList() << "h" << "height", "Viewer height",
+                             "height"};
+    parser.addOptions({opt_p, opt_w, opt_h});
+
+    if (!parser.parse(args)) {
+        qDebug() << parser.errorText();
+    }
+    bool cvt_p = false;
+    int pct    = parser.value(opt_p).toInt(&cvt_p);
+    if (cvt_p && pct > 0 && pct <= 100) {
+        if (QScreen* scn = QGuiApplication::primaryScreen()) {
+            return scn->availableSize() * pct / 100;
+        }
+    }
+    bool cvt_w = false, cvt_h = false;
+    const int width  = parser.value(opt_w).toInt(&cvt_w);
+    const int height = parser.value(opt_h).toInt(&cvt_h);
+    if (cvt_w && cvt_h) {
+        return {width, height};
+    }
+    return {};
 }
 }  // namespace seer
